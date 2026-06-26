6ba5a882eda35749e9433405e8b6decff589de5b
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-06-26 21:40:15 +0530

    feat:adding multiple contacts

--- src/main/java/com/bridgelabz/main/AddressBook.java
+++ src/main/java/com/bridgelabz/main/AddressBook.java
@@ -1,10 +1,12 @@
 package com.bridgelabz.main;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class AddressBook {
-	Contacts contact = new Contacts();
+	ArrayList<Contacts> contactList = new ArrayList<>();
 	Scanner sc = new Scanner(System.in);
-	public void add() {		
+	public void add() {	
+		Contacts contact = new Contacts();
 		System.out.println("Enter First Name: ");
 		String firstName = sc.nextLine();
 		System.out.println("Enter last Name: ");
@@ -31,30 +33,47 @@ public class AddressBook {
 		contact.setZipCode(zipCode);
 		contact.setPhoneNumber(phoneNum);
 		contact.setEmail(email);
-		System.out.println(contact);
+		contactList.add(contact);
+		System.out.println("Contact Added Successfully");
 	}
 	
 	public void editContact() {
 		System.out.println("Enter First Name of Persion");
 		String editName = sc.nextLine();
-		if (editName.equalsIgnoreCase(contact.firstName)) {
-			add();
-		} else {
-			System.out.println("Invalid Name......!");
-			System.out.println("Please enter valid First Name");
-			editContact();
-		}
-
+        for (Contacts contact : contactList) {
+            if (contact.getFirstName().equalsIgnoreCase(editName)) {
+                System.out.println("Enter New Address:");
+                contact.setAddress(sc.nextLine());
+                System.out.println("Enter New City:");
+                contact.setCity(sc.nextLine());
+                System.out.println("Enter New State:");
+                contact.setState(sc.nextLine());
+                System.out.println("Contact Updated Successfully");
+                return;
+            }
+        }
+        System.out.println("Contact Not Found");
 	}	
 	public void delete() {
 		System.out.println("Enter First Name of Person");
 		String deleteName = sc.nextLine();
-		if (deleteName.equals(contact.firstName)) {
-			System.out.println("you deleted " + contact.firstName + " data");
-			contact = null;
-		}
+        for (int i = 0; i < contactList.size(); i++) {
+            if (contactList.get(i).getFirstName().equalsIgnoreCase(deleteName)) {
+                contactList.remove(i);
+                System.out.println("Contact Deleted Successfully");
+                return;
+            }
+        }
+
+        System.out.println("Contact Not Found");
 	}
 	public void showContact() {
-		System.out.println(contact.toString());
+        if (contactList.isEmpty()) {
+            System.out.println("No Contacts Found");
+            return;
+        }
+        for (Contacts contact : contactList) {
+            System.out.println(contact);
+        }
 	}
 }
--- src/main/java/com/bridgelabz/main/AddressBookMain.java
+++ src/main/java/com/bridgelabz/main/AddressBookMain.java
@@ -5,20 +5,35 @@ public class AddressBookMain {
 		AddressBook myObj = new AddressBook();
 		System.out.println("Welcome to Address Book System");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("1.Edit \n 2.Delete \n3.ShowContacts");
-		int input = sc.nextInt();
-		switch (input) {
-		case 1:
-			myObj.editContact();
-			break;
-		case 2:
-			myObj.delete();
-			break;	
-		
-		case 3:
-			myObj.showContact();
-		    break;
-		}
-	sc.close();
+		while (true) {
+            System.out.println("\n1.Add Contact");
+            System.out.println("2.Edit Contact");
+            System.out.println("3.Delete Contact");
+            System.out.println("4.Show Contacts");
+            System.out.println("5.Exit");
+            int input = sc.nextInt();
+            sc.nextLine();
+            switch (input) {
+            case 1:
+                myObj.add();
+                break;
+            case 2:
+                myObj.editContact();
+                break;
+            case 3:
+                myObj.delete();
+                break;
+            case 4:
+                myObj.showContact();
+                break;
+            case 5:
+                System.out.println("Thank You");
+                sc.close();
+                return;
+            default:
+                System.out.println("Invalid Choice");
+            }
+        }
+	
 	}
 }
